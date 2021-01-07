1c8f5a6342c64c55db9f3e155c246b8782bba6c7
Neveo Harrison <neveoo@gmail.com>
2021-01-07 12:23:36 -0500

    Adding a bit of styling for character cards

--- src/search.js
+++ src/search.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-	const ts = '1608763962579';
-	const public_key = '3a80597230483a002fa9628a2c7774f3';
-	const uri = 'https://gateway.marvel.com/v1/public/characters?ts=';
-	const hash = 'ac71a74a1bdc232da7c76480396ca1c2';
-	const apiendpoint = uri + ts +'&apikey=' + public_key + '&hash=' + hash;
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-	const useStyles = makeStyles({
-	  root: {
-	    maxWidth: 345,
-	  },
-	  media: {
-	    height: 140,
-	  },
-	});	
+const ts = '1608763962579';
+const public_key = '3a80597230483a002fa9628a2c7774f3';
+const uri = 'https://gateway.marvel.com/v1/public/characters?ts=';
+const hash = 'ac71a74a1bdc232da7c76480396ca1c2';
+const apiendpoint = uri + ts +'&apikey=' + public_key + '&hash=' + hash;
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});	
 
 class Search extends Component {
 	state ={
@@ -42,53 +51,43 @@ class Search extends Component {
 	};
 
 	render() {
-
 		return (
 		<div className="App">
 			<h1>Marvel Universe</ h1>
 			<h2>Characters API from Marvel Universe</ h2>
 			<input 
+				required
 				name="text" 
 				type="text" 
 				placeholder="Type a character: Hulk, Iron man, Captain America, Thor etc ..."
 				onChange={event => this.handleOnChange(event)}
-				value={this.state.searchValue}
-			/>
+				value={this.state.searchValue}/>
 							
 				<button className="fetch-button" onClick={this.handleSearch}>Search</button>
-				
 
-			export default function MediaCard() {
-			  const classes = useStyles();
-
-			  return (
 			  {this.state.characters ? (
-			  	{this.state.characters.map((character, index) => (
-						<Card className={classes.root} key={index}>
+			  	this.state.characters.map((character, index) => (
+						<Card className={useStyles.root} key={index} style={{width: 400}}>
 						  <CardActionArea>
 						    <CardMedia
-						      className={classes.media}
-						      image="{character.thumbnail.path+'.'+character.thumbnail.extension}"
+						      style={{width:400,height:300}}
+						      image={character.thumbnail.path+'.'+character.thumbnail.extension}
 						      title="Marvel Characters"
 						    />
 						    <CardContent>
-						      <Typography gutterBottom variant="h5" component="h2">
+						      <Typography gutterBottom variant="h5" component="h2" align="left">
 						        {character.name}
 						      </Typography>
-						      <Typography variant="body2" color="textSecondary" component="p">
+						      <Typography variant="body2" color="textSecondary" component="p" align="left">
 						        {character.description}
 						      </Typography>
 						    </CardContent>
 						  </CardActionArea>
 						</Card>
-					))}
+					)))
 				: (
 					<p>Look up a character</p>				
 				)}
-			  );
-			}
-
-
 		</div>
 		);
 	}
