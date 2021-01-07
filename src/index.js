af38b0fc57b51597eee2462b6ef9268df618e961
Neveo Harrison <neveoo@gmail.com>
2021-01-06 21:13:38 -0500

    Adding search form to the api

--- src/index.js
+++ src/index.js
@@ -1,48 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
+import Search from './search';
 import './styles.css';
 
-
-function App(){
-	const [characters, setCharacters] = useState("");
-	const ts = '1608763962579';
-	const public_key = '3a80597230483a002fa9628a2c7774f3';
-	const uri = 'https://gateway.marvel.com/v1/public/characters?ts=';
-	const hash = 'ac71a74a1bdc232da7c76480396ca1c2';
-	const apiendpoint = uri + ts +'&apikey=' + public_key + '&hash=' + hash;
-
-	const fetchData = () => {
-		axios.get(apiendpoint).then((response) => {
-
-			console.log(response.data.data.results);
-			setCharacters(response.data);
-
-			}
-		);
-	};
-
-	return(
-	<div className="App">
-		<h1>Marvel Universe</ h1>
-		<h2>Fetch a list from Marvel characters from API</ h2>
-
-		<div>
-			<button className="fetch-button" onClick={fetchData}>
-			Fetch Data 
-			</ button>
-			<br />
-		</div>
-
-		<div className="characters">
-
-		</div>
-
-
-
-	</div>
-	);
-}
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Search />, document.getElementById('root'));
---
+++ src/search.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+	const ts = '1608763962579';
+	const public_key = '3a80597230483a002fa9628a2c7774f3';
+	const uri = 'https://gateway.marvel.com/v1/public/characters?ts=';
+	const hash = 'ac71a74a1bdc232da7c76480396ca1c2';
+	const apiendpoint = uri + ts +'&apikey=' + public_key + '&hash=' + hash;
+
+class Search extends Component {
+	state ={
+		searchValue: "",
+		characters: []
+	};
+
+	handleOnChange = event => {
+		this.setState({ searchValue: event.target.value});
+	};
+
+	handleSearch = () => {
+		this.makeApiCall(this.state.searchValue);
+	};
+
+	makeApiCall = searchInput => {
+		const searchUri = apiendpoint +'?s={searchInput}';
+		fetch(searchUri)
+			.then(response => {
+				return response.json();
+			})
+			.then(jsonData => {
+				this.setState({ characters: jsonData.characters});
+			});
+	};
+
+	render() {
+
+		return (
+
+
+		<div className="App">
+			<h1>Marvel Universe</ h1>
+			<h2>Fetch a list from Marvel characters from API</ h2>
+			<input 
+				name="text" 
+				type="text" 
+				placeholder="Type in a Marvel character you looking for..."
+				onChange={event => this.handleOnChange(event)}
+				value={this.state.searchValue}
+			/>
+							
+				<button className="fetch-button" onClick={this.handleSearch}>Search</button>
+				{this.state.characters ? (
+					<div>
+					{this.state.characters.map((character, index) => (
+						<div className="single-character" key={index}>
+							<h2>{character.name}</h2>
+							<p>{character.description}</p>
+						</div>
+					))}
+					</div>
+				) : (
+					<p>Look up a character</p>				
+				)}
+		</div>
+		);
+	}
+}
+
+export default Search;
