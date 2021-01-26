1f104f7bb7fa2eb7b643aad2db13ca046c8a073c
Neveo Harrison <neveoo@gmail.com>
2021-01-26 01:01:33 -0500

    Adding more functionality to setup for grabbing data

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,6 +4,16 @@ import Loading from './Loading';
 import { useGlobalContext } from '../context';
 
 const CharacterList = () => {
+    const { characters, loading } = useGlobalContext();
+
+    if(loading){
+        return <Loading />
+    }
+    if(characters.length < 1 ){
+        return(
+            <h2 className="section-title">no character match that search.</h2>
+        );
+    }
     return (
         <div>
             <h2>character list</h2>
